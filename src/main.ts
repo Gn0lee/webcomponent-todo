32d3af4fbb3210afb333eb7cca6b0366c2762e0a
Gn0lee <jinho605@gmail.com>
2025-01-15 23:55:20 +0900

    declare custom element

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <h1>Hello Vite!</h1>
-`;
+import { TodoItemComponent } from "./components/todoItemComponent";
+import { TodoListComponent } from "./components/todoListComponent";
+
+window.customElements.define("todo-item", TodoItemComponent);
+window.customElements.define("todo-list", TodoListComponent);
